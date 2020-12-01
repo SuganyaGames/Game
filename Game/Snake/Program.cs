0fc2437d7f50674b69e96e51ce66489aaac60576
BC226H2-OOBE-01\venkat <suganyapselvam@gmail.com>
2020-12-01 19:37:52 +0530

    I wrote Snake GAme

---
+++ Game/Snake/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace Snake
+{
+    class Program
+    {
+        static List<Snake> snakes { get; set; }
+        static void Main(string[] args)
+        {
+            Console.BufferWidth = Console.WindowWidth = 80;
+            Console.BufferHeight = Console.WindowHeight = 30;
+            ConsoleKey Direction = ConsoleKey.RightArrow;
+            var sneaks = SnakePrint();
+            sneaks.ForEach(x => x.Print());
+            var snakeFood = FoodPoint();
+            snakeFood.Print();
+            int Point = 50;
+            int Time = 0;
+            while (Point > 0)
+            {
+                while (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo PressedKey = Console.ReadKey(true);
+                    Direction = PressedKey.Key;
+                }
+
+                var snake = KeyAvailable(Direction);
+                snake.Print();
+                if(snakes.Any(x=>x.X==snake.X && x.Y == snake.Y))
+                {
+                    //Point = Point - 10;
+                    break;
+                }
+                snakes.Insert(snakes.Count, snake);
+                if (!GetFood(snake, snakeFood))
+                {
+                    snakes[0].Clear();
+                    snakes.RemoveAt(0);
+                }
+                else
+                {
+                    snakeFood = FoodPoint();
+                    snakeFood.Print();
+                }
+                var snakesDangerous = DangerousPoint();
+                snakesDangerous.ForEach(x => x.Print());
+                if (Time > 50)
+                {
+                    snakeFood.Clear();
+                    snakeFood = FoodPoint();
+                    snakeFood.Print();
+                    Time = 00;
+                }
+                var hitsnake = HitSnake(snakesDangerous, snake);
+                Point = hitsnake ? Point -= 10 : Point;
+                Time++;
+                Print_Time_point(Time, Point);
+                Thread.Sleep(300);
+            }
+        }
+        static List<Snake> SnakePrint()
+        {
+            snakes = new List<Snake>();
+            snakes.Add(new Snake(15, 20, SnakeTypes.Snake));
+            snakes.Add(new Snake(16, 20, SnakeTypes.Snake));
+            snakes.Add(new Snake(17, 20, SnakeTypes.Snake));
+            snakes.Add(new Snake(18, 20, SnakeTypes.Snake));
+            return snakes;
+        }
+        static Snake KeyAvailable(ConsoleKey Direction)
+        {
+            var Head = snakes[snakes.Count - 1];
+            var Tail = new Snake(snakes[0].X, snakes[0].Y, SnakeTypes.Snake);
+            if (Direction == ConsoleKey.LeftArrow)
+            {
+                Tail.X = Head.X - 1;
+                Tail.Y = Head.Y;
+                //Tail.X = Tail.X <= 0 ? Console.WindowWidth - 1 : Tail.X;
+            }
+            else if (Direction == ConsoleKey.RightArrow)
+            {
+                Tail.X = Head.X + 1;
+                Tail.Y = Head.Y;
+                //Tail.X = Tail.X >= Console.WindowWidth - 1 ? 0 : Tail.X;
+            }
+            else if (Direction == ConsoleKey.UpArrow)
+            {
+                Tail.Y = Head.Y - 1;
+                Tail.X = Head.X;
+                //Tail.Y = Tail.Y <= 0 ? Console.WindowHeight - 1 : Tail.Y;
+            }
+            else if (Direction == ConsoleKey.DownArrow)
+            {
+                Tail.Y = Head.Y + 1;
+                Tail.X = Head.X;
+                //Tail.Y = Tail.Y >= Console.WindowHeight - 1 ? 0 : Tail.Y;
+            }
+            return Tail;
+        }
+        static List<Snake> DangerousPoint()
+        {
+            var snakes = new List<Snake>();
+            snakes.Add(new Snake(20, 15, SnakeTypes.Dangerous));
+            snakes.Add(new Snake(15, 15, SnakeTypes.Dangerous));
+            snakes.Add(new Snake(5, 7, SnakeTypes.Dangerous));
+            snakes.Add(new Snake(10, 25, SnakeTypes.Dangerous));
+            return snakes;
+        }
+        static Snake FoodPoint()
+        {
+            Random rant = new Random();
+            int X = rant.Next(1, 50);
+            int Y = rant.Next(1, 30);
+            SnakeTypes snakeType = SnakeTypes.Food;
+            Snake snake = new Snake(X, Y, snakeType);
+            return snake;
+        }
+        static bool GetFood(Snake snake, Snake snakeFood)
+        {
+            if (snake.X == snakeFood.X && snake.Y == snakeFood.Y)
+            {
+                return true;
+            }
+            return false;
+        }
+        static bool HitSnake(List<Snake> dangerous, Snake snake)
+        {
+            Random rant = new Random();
+            int X = rant.Next(1, 80);
+            int Y = rant.Next(1, 30);
+            SnakeTypes snakeType = SnakeTypes.Dangerous;
+            if (dangerous.Any(x => x.X == snake.X && x.Y == snake.Y))
+            {
+                var danger = new Snake(X, Y, snakeType);
+                danger.Print();
+                dangerous.Add(danger);
+                return true;
+            }
+            return false;
+        }
+        static void Print_Time_point(int Time, int Point)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(50, 10);
+            Console.WriteLine($"TIme is: {Time}  ");
+            Console.SetCursorPosition(50, 12);
+            Console.WriteLine($"Point is: {Point}");
+        }
+    }
+}
---
+++ Game/Snake/Snake.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class Snake
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public SnakeTypes Type { get; set; }
+        public Snake(int x,int y,SnakeTypes type)
+        {
+            X = x;
+            Y = y;
+            Type = type;
+        }
+        public char GetSymbol()
+        {
+            return SnakeTypes.Snake == Type ? '*' : (SnakeTypes.Food == Type) ? '*' : (SnakeTypes.Dangerous == Type) ? '#' : ' '; 
+        }
+        public ConsoleColor GetColor()
+        {
+            if (Type == SnakeTypes.Snake)
+            {
+                return ConsoleColor.DarkCyan;
+            }
+            else if (Type == SnakeTypes.Food)
+            {
+                return ConsoleColor.Yellow;
+            }
+            else
+            {
+                return ConsoleColor.DarkMagenta;
+            }
+        }
+        public void Print()
+        {
+            Console.SetCursorPosition(X, Y);
+            Console.ForegroundColor = GetColor();
+            Console.WriteLine(GetSymbol());
+        }
+        public void Clear()
+        {
+            Console.SetCursorPosition(X, Y);
+            Console.WriteLine(" ");
+        }
+    }
+}
